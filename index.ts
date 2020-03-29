ac4b43cf18beabcf54f2ed242a3e51cfdb9890d8
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-29 17:29:33 +0200

    Read currentUserId from cookie

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
 import http from 'http'
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import cookie from 'cookie'
 import { ApolloServer, PubSub } from 'apollo-server-express'
 import { users } from './db'
 import schema from './schema'
 
 const app = express()
 
-app.use(cors())
+const origin = process.env.ORIGIN || 'http://localhost:3000'
+app.use(cors({ credentials: true, origin }))
+
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/_ping', (req, res) => {
   res.send('pong')
@@ -17,15 +22,36 @@ app.get('/_ping', (req, res) => {
 const pubsub = new PubSub()
 const server = new ApolloServer({
   schema,
-  context: () => ({
-    currentUser: users.find((u) => u.id === '1'),
-    pubsub,
-  }),
+  context: (session: any) => {
+    // Access the request object
+    let req = session.connection
+      ? session.connection.context.request
+      : session.req
+
+    // It's subscription
+    if (session.connection) {
+      req.cookies = cookie.parse(req.headers.cookie || '')
+    }
+
+    return {
+      currentUser: users.find((u) => u.id === req.cookies.currentUserId),
+      pubsub,
+    }
+  },
+  subscriptions: {
+    onConnect(params, ws, ctx) {
+      // pass the request object to context
+      return {
+        request: ctx.request,
+      }
+    },
+  },
 })
 
 server.applyMiddleware({
   app,
   path: '/graphql',
+  cors: { credentials: true, origin },
 })
 
 const httpServer = http.createServer(app)
