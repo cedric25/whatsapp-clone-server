c8affd6c7ab45f090c702067b216506276ec272c
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-29 14:46:22 +0200

    Add Subscription part in resolvers

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -58,6 +58,13 @@ const resolvers: Resolvers = {
       return message
     },
   },
+
+  Subscription: {
+    messageAdded: {
+      subscribe: (root, args, { pubsub }) =>
+        pubsub.asyncIterator('messageAdded'),
+    },
+  },
 }
 
 export default resolvers
