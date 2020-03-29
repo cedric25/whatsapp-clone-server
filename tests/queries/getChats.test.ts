07bc78ae14383cf3438af95717fa4e324e0a74bd
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-29 17:09:07 +0200

    More stuff around users

--- tests/queries/getChats.test.ts
+++ tests/queries/getChats.test.ts
@@ -1,15 +1,19 @@
 import { createTestClient } from 'apollo-server-testing'
 import { ApolloServer, gql } from 'apollo-server-express'
 import schema from '../../schema'
+import { users } from '../../db'
 
 // jest.spyOn(Date, 'now')
 //   .mockImplementation(() => new Date('2020-03-26T10:00:00.000Z').getTime())
 
 describe('Query.chats', () => {
   it('should fetch all chats', async () => {
-    const server = new ApolloServer({ schema })
-
-    console.log('Date.now()', Date.now())
+    const server = new ApolloServer({
+      schema,
+      context: () => ({
+        currentUser: users[0],
+      }),
+    })
 
     const { query } = createTestClient(server)
 
