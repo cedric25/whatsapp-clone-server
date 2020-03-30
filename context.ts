2469f6c10bc0729fa8c01f8ae03c29ea66d330df
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2020-03-30 19:41:43 +0200

    Init connection pool to postgres + get user from db

--- context.ts
+++ context.ts
@@ -1,9 +1,11 @@
 import { PubSub } from 'apollo-server-express'
 import { User } from './db'
 import { Response } from 'express'
+import { PoolClient } from 'pg'
 
 export type MyContext = {
   currentUser: User
   pubsub: PubSub
   res: Response
+  db: PoolClient
 }
